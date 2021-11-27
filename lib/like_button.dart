c454f09090f1bd085f578c9748405201cf0341d1
Danzo7 <aymenmiro@gmail.com>
2021-11-27 15:07:18 +0100

    :zap: export TapController

--- lib/like_button.dart
+++ lib/like_button.dart
@@ -3,3 +3,4 @@ library like_button;
 export 'src/like_button.dart';
 export 'src/utils/like_button_model.dart';
 export 'src/utils/like_button_typedef.dart';
+export 'src/utils/tap_controller.dart';
